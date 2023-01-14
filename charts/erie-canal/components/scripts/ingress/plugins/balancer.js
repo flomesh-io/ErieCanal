2b1ed610bdaf8a3131de7fcfb2324bea6785a4f4
Lin Yang <reaver@flomesh.io>
2023-01-14 12:10:12 +0800

    feat: session sticky
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- charts/erie-canal/components/scripts/ingress/plugins/balancer.js
+++ charts/erie-canal/components/scripts/ingress/plugins/balancer.js
@@ -35,15 +35,19 @@
       Object.fromEntries(
         Object.entries(ingress.services).map(
           ([k, v]) =>(
-            ((targets, balancer) => (
+            ((targets, balancer, balancerInst) => (
               targets = v?.upstream?.endpoints?.map(ep => `${ep.ip}:${ep.port}`),
               v?.upstream?.sslCert?.ca && (
                 addUpstreamIssuingCA(v.upstream.sslCert.ca)
               ),
               balancer = balancers[v?.balancer || 'round-robin'] || balancers['round-robin'],
+              balancerInst = new balancer(targets || []),
 
               [k, {
-                balancer: new balancer(targets || []),
+                balancer: balancerInst,
+                cache: v?.sticky && new algo.Cache(
+                  () => balancerInst.next()
+                ),
                 upstreamSSLName: v?.upstream?.sslName || null,
                 upstreamSSLVerify: v?.upstream?.sslVerify || false,
                 cert: v?.upstream?.sslCert?.cert,
@@ -63,36 +67,82 @@
     _serviceCertChain: null,
     _servicePrivateKey: null,
     _connectTLS: null,
+
+    _serviceCache: null,
+    _targetCache: null,
+
+    _sourceIP: null,
+
+    _g: {
+      connectionID: 0,
+    },
+
+    _connectionPool: new algo.ResourcePool(
+      () => ++_g.connectionID
+    ),
+    
+    _select: (service, key) => (
+      service?.cache && key ? (
+        service?.cache?.get(key)
+      ) : (
+        service?.balancer?.next()
+      )
+    ),
   })
 
-    .import({
-      __route: 'main',
-    })
+  .import({
+    __route: 'main',
+  })
 
-    .pipeline()
+  .pipeline()
+    .handleStreamStart(
+      () => (
+        _serviceCache = new algo.Cache(
+          // k is a balancer, v is a target
+          (k) => _select(k, _sourceIP),
+          (k, v) => k.balancer.deselect(v),
+        ),
+        _targetCache = new algo.Cache(
+          // k is a target, v is a connection ID
+          (k) => _connectionPool.allocate(k),
+          (k, v) => _connectionPool.free(v),
+        )
+      )
+    )
+    .handleStreamEnd(
+      () => (
+        _targetCache.clear(),
+        _serviceCache.clear()
+      )
+    )
+    .link('outbound-http')
+
+  .pipeline('outbound-http')
     .handleMessageStart(
       (msg) => (
+        _sourceIP = __inbound.remoteAddress,
         _service = services[__route],
         _service && (
           _serviceSNI = _service?.upstreamSSLName,
           _serviceVerify = _service?.upstreamSSLVerify,
           _serviceCertChain = _service?.cert,
           _servicePrivateKey = _service?.key,
-          _target = _service?.balancer?.next?.()
+          _target = _serviceCache.get(_service)
         ),
         _connectTLS = Boolean(_serviceCertChain) && Boolean(_servicePrivateKey),
 
+        console.log("[balancer] _sourceIP", _sourceIP),
         console.log("[balancer] _connectTLS", _connectTLS),
         console.log("[balancer] _target.id", (_target || {id : ''}).id)
       )
     )
     .branch(
       () => Boolean(_target) && !Boolean(_connectTLS), (
-        $=>$.muxHTTP(() => _target).to(
+        $=>$.muxHTTP(() => _targetCache.get(_target)).to(
           $=>$.connect(() => _target.id)
         )
       ), () => Boolean(_target) && Boolean(_connectTLS), (
-        $=>$.muxHTTP(() => _target).to(
+        $=>$.muxHTTP(() => _targetCache.get(_target)).to(
           $=>$.connectTLS({
             certificate: () => ({
               cert: new crypto.Certificate(_serviceCertChain),
