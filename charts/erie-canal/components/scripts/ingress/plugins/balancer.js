31bcd7ffcf0aa890628f265462004bb33ff246cb
Lin Yang <reaver@flomesh.io>
2023-02-06 22:54:48 +0800

    refactor: add log
    Signed-off-by: Lin Yang <reaver@flomesh.io>

--- charts/erie-canal/components/scripts/ingress/plugins/balancer.js
+++ charts/erie-canal/components/scripts/ingress/plugins/balancer.js
@@ -133,7 +133,7 @@
         _mTLS = _connectTLS && Boolean(_serviceCertChain) && Boolean(_servicePrivateKey),
 
         console.log("[balancer] _sourceIP", _sourceIP),
-        console.log("[balancer] _connectTLS", _connectTLS),
+        console.log("[balancer] _connectTLS", _connectTLS, "_mTLS", _mTLS),
         console.log("[balancer] _target.id", (_target || {id : ''}).id)
       )
     )
